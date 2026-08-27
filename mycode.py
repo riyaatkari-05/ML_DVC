d4e7d1657927cba4210e15c31c244ef1ca744312
Riya <riya.atkari@somaiya.edu>
2026-08-27 14:39:49 +0530

    Data version 3

--- mycode.py
+++ mycode.py
@@ -1,12 +1,12 @@
 import os
 import csv
-
 os.makedirs("data", exist_ok=True)
 
 file_path = "data/data.csv"
-
 with open(file_path, "a", newline="") as file:
     writer = csv.writer(file)
     writer.writerow([4, "David", 92])
+    writer.writerow([5, "Eve", 88])
 
 print("New data added successfully.")
+
